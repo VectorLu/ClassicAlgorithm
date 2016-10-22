6815fab629bc673ca8af9b088134243d9aff5e9f
VectorLu <vectorpinglu@outlook.com>
2016-10-22 10:16:52 +0800

    move codes to 1.1_1.2

---
+++ Algorithm4th/CH1/1.3/Stats.java
@@ -0,0 +1,10 @@
+public class Stats
+{
+    public static void main(String[] args) {
+        Bag<Double> numbers = new Bag<Double>();
+        while(!StdIn.isEmpty)
+        {
+            
+        }
+    }
+}
