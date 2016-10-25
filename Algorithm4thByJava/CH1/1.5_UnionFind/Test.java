caa1339907c4be23907c3ec276b3782ec53e2169
VectorLu <vectorpinglu@outlook.com>
2016-10-25 11:17:49 +0800

    Weighted Quick Union Find

---
+++ Algorithm4thByJava/CH1/1.5_UnionFind/Test.java
@@ -0,0 +1,8 @@
+import edu.princetion.cs.algs4.*;
+
+public class Test
+{
+    public static void main(String[] args) {
+
+    }
+}
--- Algorithm4thByJava/CH1/1.5_UnionFind/WeightedQuickUnionUF.java
+++ Algorithm4thByJava/CH1/1.5_UnionFind/WeightedQuickUnionUF.java
@@ -9,7 +9,43 @@ public class WeightedQuickUnionUF
     {
         count = n;
         id = new id[n];
-        for (int )
+        for (int i = 0; i < n; i++)
+        {id[i] = i;}
+        for (int i = 0; i < n; i++)
+        {size[i] = 1;}
     }
 
+    public int count()
+    {return count;}
+
+    public boolean connected(int p, int q)
+    {return find(p)==find(q);}
+
+    private int find(int p)
+    {   //Follow links to find a root.
+        while(p != id[p])
+        {p = id[p];}
+        return p;
+    }
+
+    public void union(int p, int q)
+    {
+        int pRoot = find(p);
+        int qRoot = find(q);
+        if (pRoot == qRoot){return;}
+
+        // Make smaller root point to the larger one.
+        if (size[pRoot] < size[qRoot])
+        {
+            id[pRoot] = qRoot;
+            size[qRoot] += size[pRoot];
+        }
+        else
+        {
+            id[qRoot] = pRoot;
+            size[pRoot] += size[qRoot];
+        }
+
+        count--;
+    }
 }
