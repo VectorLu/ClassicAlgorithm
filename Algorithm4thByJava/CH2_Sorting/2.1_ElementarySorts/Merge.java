b5e694cdbd01a7f233d26d4018907ab00698492b
VectorLu <vectorpinglu@outlook.com>
2016-10-27 19:16:43 +0800

    merge sort

--- Algorithm4thByJava/CH2_Sorting/2.1_ElementarySorts/Merge.java
+++ Algorithm4thByJava/CH2_Sorting/2.1_ElementarySorts/Merge.java
@@ -8,7 +8,16 @@ public class Merge
         sort(a, 0, a.length-1);
     }
 
-    public static void merge(Comparable[] a, int lo, int mid, int hi)
+    private static void sort(Comparable[] a, int lo, int hi)
+    {
+        if (hi <= lo) {return;}
+        int mid = lo + (hi - lo)/2;
+        sort(a, lo, mid);
+        sort(a, mid+1, hi);
+        merge(a, lo, mid, hi);
+    }
+
+    private static void merge(Comparable[] a, int lo, int mid, int hi)
     {   //Merge a[lo..mid] with a[mid+1..hi]
         int i = lo, j = mid + 1;
 
@@ -17,15 +26,10 @@ public class Merge
 
         for (int k = lo; k <= hi; k++) //Merge back to a[lo..hi].
         {
-            if (i > mid){a[k] = aux[j++];}
-            else ()
+            if (i > mid) {a[k] = aux[j++];}
+            else if (j > hi) {a[k] = aux[i++];}
+            else if (less(a[j], a[i])) {a[k] = aux[j++];}
+            else {a[k] = aux[i++];}
         }
-    }
-
-    private static void sort(Comparable[] a, int lo, int hi)
-    {
-        if (hi <= lo){return;}
-        int mid = lo + (hi - lo)/2;
-
     }
 }
