baf6502e14d1550d95dca025b3b65d92b785048d
VectorLu <vectorpinglu@outlook.com>
2016-10-28 10:26:33 +0800

    test quick sort

--- Algorithm4thByJava/CH2_Sorting/2.3_QuickSort/Quick.java
+++ Algorithm4thByJava/CH2_Sorting/2.3_QuickSort/Quick.java
@@ -36,4 +36,40 @@ public class Quick
         exch(a, lo, j);     // Put v = a[j] into position.
         return j;           // with a[lo..j-1] <= a[j] <= a[j+1..hi]
     }
+
+    private static boolean less(Comparable v, Comparable w)
+    {return v.compareTo(w) < 0;}
+
+    private static void exch(Comparable[] a, int i, int j)
+    {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show(Comparable[] a)
+    {   //Print the array, on a single line.
+        for (int i = 0; i < a.length; i++)
+        {
+            StdOut.println(a[i]);
+        }
+        StdOut.println();
+    }
+
+    public static boolean isSorted(Comparable[] a)
+    {   //Test whether the array entries are in order.
+        for (int i = 1; i < a.length; i++)
+        {
+            if (less(a[i], a[i-1])) {return false;}
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        //Read strings from the standard input, sort them, and print them.
+        String[] a = In.readStrings();
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
 }
