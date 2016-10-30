b88e0fde8b1bcbef6744a63b92a014dadea17efe
VectorLu <vectorpinglu@outlook.com>
2016-10-30 19:39:58 +0800

    priority queue client

---
+++ Algorithm4thByJava/CH2_Sorting/2.4_PriorityQueue/TopM.java
@@ -0,0 +1,24 @@
+public class TopM
+{
+    public static void main(String[] args) {
+        // Print the top M lines in the input stream.
+        int M = Integer.parseInt(args[0]);
+        MinPQ<Transaction> pq ＝ new MinPQ<Transaction>(M+1);
+        while (StdIn.hasNextLine())
+        {   // Create an entry from the next line and put on the PQ.
+            pq.insert(new Transaction(StdIn.readLine()));
+            if (pq.size() > M)
+            {pq.delMin();}  // Remove minimum if M+1 entries on the PQ.
+        }   // Top M entries are on the PQ.
+
+        Stack<Transaction> stack = new Stack<Transaction>();
+        while (!pq.isEmpty())
+        {
+            stack.push(pq.delMin());
+        }
+        for (Transaction t : stack)
+        {
+            StdOut.println(t);
+        }
+    }
+}
