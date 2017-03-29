3e4cae9c94bb1aa48fff244894c50b3cb63c2110
VectorLu <vectorpinglu@outlook.com>
2017-03-29 16:50:33 +0800

    Algorithm time consume

---
+++ PythonAlgorithm/useTimeitModule.py
@@ -0,0 +1,39 @@
+def test1():
+    l = []
+    for i in range(1000):
+        l = l + [i]
+
+def test2():
+    l = []
+    for i in range(1000):
+        l.append(i)
+
+def test3():
+    l = [i for i in range(1000)]
+
+def test4():
+    l = list(range(1000))
+
+import timeit
+from timeit import Timer
+t1 = Timer('test1()', 'from __main__ import test1')
+print('concat ', t1.timeit(number = 1000), 'milliseconds')
+t2 = Timer("test2()", "from __main__ import test2")
+print("append ",t2.timeit(number=1000), "milliseconds")
+t3 = Timer("test3()", "from __main__ import test3")
+print("comprehension ",t3.timeit(number=1000), "milliseconds")
+t4 = Timer("test4()", "from __main__ import test4")
+print("list range ",t4.timeit(number=1000), "milliseconds")
+
+popzero = Timer('x.pop(0)', 'from __main__ import x')
+popend = Timer('x.pop()', 'from __main__ import x')
+
+# x = list(range(2000000))
+# print(popzero.timeit(number = 1000))
+# print(popend.timeit(number = 1000))
+print('pop(0)                   pop()')
+for i in range(1000000,100000001,1000000):
+    x = list(range(i))
+    pt = popend.timeit(number = 1000)
+    pz = popzero.timeit(number = 1000)
+    print('%15.5f, %15.5f' % (pz, pt))
