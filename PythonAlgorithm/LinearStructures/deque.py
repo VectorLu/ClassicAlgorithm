4e89e0c4aba3df2b2c9b13bde57b9b95115ac758
VectorLu <vectorpinglu@outlook.com>
2017-04-03 21:58:12 +0800

    LinkedList

--- PythonAlgorithm/LinearStructures/deque.py
+++ PythonAlgorithm/LinearStructures/deque.py
@@ -8,4 +8,33 @@ class Deque:
     def addFront(self, item):
         self.items.append(item)
 
-    
+    def addRear(self, item):
+        self.items.insert(0, item)
+
+    def removeFront(self):
+        return self.items.pop()
+
+    def removeRear(self):
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
+
+
+def palindromeChecker(aStr):
+    charDeque = Deque()
+
+    for ch in aStr:
+        charDeque.addRear(ch)
+    stillEqual = True
+
+    while charDeque.size() > 1 and stillEqual:
+        first = charDeque.removeFront()
+        last = charDeque.removeRear()
+        if first != last:
+            stillEqual = False
+
+    return stillEqual
+
+print(palindromeChecker('lsdkjfskf'))
+print(palindromeChecker('radar'))
---
+++ PythonAlgorithm/LinearStructures/linkedList.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, initData):
+        self.data = initData
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self, newData):
+        self.data = newData
+
+    def setNext(self, newNext):
+        self.next = newNext
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        return self.head == None
+
+    def add(self, item):
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.getNext()
+        return count
+
+    def search(self, item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext
+
+        return found
