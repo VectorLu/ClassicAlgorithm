6f623f98ac1c3c5eb10cc67394592375d401c61d
VectorLu <vectorpinglu@outlook.com>
2017-04-14 10:26:02 +0800

    add comment

--- PythonAlgorithm/tree/binarySearchTree.py
+++ PythonAlgorithm/tree/binarySearchTree.py
@@ -57,6 +57,9 @@ class BinarySearchTree:
     def __len__(self):
         return self.size
 
+    '''
+    One important problem with our implementation of insert is that duplicate keys are not kandled properly. As our tree is implemented a duplicate key will create a new node with the same key value in the right sub-tree of the node having the original key.
+    '''
     def put(self, key, val):
         if self.root:
             self._put(key, val,self.root)
@@ -94,4 +97,61 @@ class BinarySearchTree:
             return None
         elif currentNode.key == key:
             return currentNode
-        
+        elif key < currentNode.key:
+            return self._get(key, currentNode.leftChild)
+        else:
+            return self._get(key, currentNode.rightChild)
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __contains__(self, key):
+        if self._get(key, self.root):
+            return True
+        else:
+            return False
+
+    def delete(self, key):
+        if self.size > 1:
+            nodeToRemove = self._get(key, self.root)
+            if nodeToRemove:
+                self.remove(nodeToRemove)
+                self.size -= 1
+            else:
+                raise KeyError('Error, key not in tree')
+        elif self.size == 1 and self.root.key == key:
+            self.root = None
+            self.size -= 1
+        else:
+            raise KeyError('Error, key not in tree')
+
+    def __delitem__(self, key):
+        self.delete(key)
+
+    def spliceOut(self):
+        if self.isLeaf():
+            if self.isLeftChild():
+                self.parent.leftChild = None
+            else:
+                self.parent.rightChild = None
+        elif self.hasAnyChildren():
+            if self.hasLeftChild():
+                if self.isLeftChild():
+                    self.parent.leftChild = self.leftChild
+                else:
+                    self.parent.rightChild = self.leftChild
+                self.leftChild.parent = self.parent
+            else:
+                if self.isLeftChild():
+                    self.parent.leftChild = self.rightChild
+                else:
+                    self.parent.rightChild = self.rightChild
+                self.rightChild.parent = self.parent
+
+    def findSuccessor(self):
+        succ = None
+        if self.hasRightChild():
+            succ = self.rightChild.findMin()
+        else:
+            if self.parent:
+                if self.isLeftChild():
