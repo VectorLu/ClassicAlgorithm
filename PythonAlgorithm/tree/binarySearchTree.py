8e8030c1bf9a9cd25a95b9720016fe29d7075c04
VectorLu <vectorpinglu@outlook.com>
2017-04-14 10:25:51 +0800

    Try VS Code git

--- PythonAlgorithm/tree/binarySearchTree.py
+++ PythonAlgorithm/tree/binarySearchTree.py
@@ -64,4 +64,34 @@ class BinarySearchTree:
             self.root = TreeNode(key, val)
         self.size = self.size + 1
 
-    
+    def _put(self, key, val, currentNode):
+        if key < currentNode.key:
+            if currentNode.hasLeftChild():
+                self._put(key, val, currentNode.leftChild)
+            else:
+                currentNode.leftChild = TreeNode(key, val, parent=currentNode)
+        else:
+            if currentNode.hasRightChild():
+                self._put(key, val, currentNode.rightChild)
+            else:
+                currentNode.rightChild = TreeNode(key, val, parent=currentNode)
+
+    def __setitem__(self, k, v):
+        self.put(k, v)
+
+    def get(self, key):
+        if self.root:
+            res = self._get(key, self.root)
+            if res:
+                return res.payload
+            else:
+                return None
+        else:
+            return None
+
+    def _get(self, key, currentNode):
+        if not currentNode:
+            return None
+        elif currentNode.key == key:
+            return currentNode
+        
