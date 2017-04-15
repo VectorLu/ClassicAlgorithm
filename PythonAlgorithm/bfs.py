e3ed0c7c7db2aeb752e9c25849173b4b9aa172ef
VectorLu <vectorpinglu@outlook.com>
2017-04-15 12:56:51 +0800

    Breadth First Search

--- PythonAlgorithm/bfs.py
+++ PythonAlgorithm/bfs.py
@@ -1,2 +1,18 @@
 from lib.graphs import Graph, Vertex
 from lib.queue import Queue
+
+def bfs(g, start):
+    start.setDistance(0)
+    start.setPred(None)
+    vertQueue = Queue()
+    vertQueue.enqueue(start)
+    while (vertQueue.size() > 0):
+        currentVert = vertQueue.dequeue()
+        for nbr in currentVert.getConnections():
+            if (nbr.getColor() == 'white'):
+                nbr.setColor('gray')
+                nbr.setDistance(currentVert.getDistance() + 1)
+                nbr.setPred(currentVert)
+                vertQueue.enqueue(nbr)
+        currentVert.setColor('black')
+        
