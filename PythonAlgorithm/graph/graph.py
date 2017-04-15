b9bb4035bca4bed7fe69b9620fe3c1562e9717be
VectorLu <vectorpinglu@outlook.com>
2017-04-15 10:40:47 +0800

    Word Ladder

---
+++ PythonAlgorithm/graph/graph.py
@@ -0,0 +1,92 @@
+class Vertex:
+    def __init__(self, key):
+        self.id = key
+        self.connectedTo = {}
+
+    def addNeighbor(self, nbr, weight = 0):
+        self.connectedTo[nbr] = weight
+
+    def __str__(self):
+        return str(self.id) + ' connectedTo: ' + str([x.id for x in self.connectedTo])
+
+    def getConnections(self):
+        return self.connectedTo.keys()
+
+    def getId(self):
+        return self.id
+
+    def getWeight(self, nbr):
+        return self.connectedTo[nbr]
+
+class Graph:
+    def __init__(self):
+        self.vertList = {}
+        self.numVertices = 0
+
+    def addVertex(self, key):
+        self.numVertices += 1
+        newVertex = Vertex(key)
+        self.vertList[key] = newVertex
+        return newVertex
+
+    def getVertex(self, key):
+        if key in self.vertList:
+            return self.vertList[key]
+        else:
+            return None
+
+    def __contains__(self, key):
+        return key in self.vertList
+
+    def addEdge(self, f, t, weight=0):
+        if f not in self.vertList:
+            self.addVertex(f)
+        if t not in self.vertList:
+            self.addVertex(t)
+        self.vertList[f].addNeighbor(self.vertList[t], weight)
+
+    def getVertices(self):
+        return self.vertList.keys()
+
+    def  __iter__(self):
+        return iter(self.vertList.values())
+
+# Word Ladder
+def buildGraph(wordFile):
+    d = {}
+    g = Graph()
+    wfile = open(wordFile, 'r')
+    # Create buckets of words that differ by one letter
+    for line in wfile:
+        word = line[:-1]
+        for i in range(len(word)):
+            bucket = word[:i] + '_' + word[i+1:]
+            if bucket in d:
+                d[bucket].append(word)
+            else:
+                d[bucket] = [word]
+    # Add vertices and edges for words in the same bucket
+    for bucket in d.keys():
+        for word1 in d[bucket]:
+            for word2 in d[bucket]:
+                if word1 != word2:
+                    g.addEdge(word1, word2)
+    return g                
+
+
+g = Graph()
+for i in range(6):
+    g.addVertex(i)
+g.addEdge(0, 1, 5)
+g.addEdge(0, 5, 2)
+g.addEdge(1, 2, 4)
+g.addEdge(2, 3, 9)
+g.addEdge(3, 4, 7)
+g.addEdge(3, 5, 3)
+g.addEdge(4, 0, 1)
+g.addEdge(5, 4, 8)
+g.addEdge(5, 2, 1)
+
+for v in g:
+    for w in v.getConnections():
+        print('(%s, %s)' % (v.getId(), w.getId()))
